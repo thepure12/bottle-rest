3116a0e1973b5965516d6daa011acba3c6158ce9
TJ Renninger <renningert@LAPTOP-LLEJQRTO.localdomain>
2023-04-20 10:40:41 -0400

    added rule tracking

--- bottle_rest.py
+++ bottle_rest.py
@@ -1,10 +1,10 @@
 from enum import Enum
-from typing import Type
-from bottle import PluginError, Route, Bottle, abort, request, response
+from typing import Type, Union
+from ..bottle import PluginError, Route, Bottle, abort, request, response
 from inspect import signature
 
-class Resource(object):
 
+class Resource(object):
     class Methods(Enum):
         GET = "GET"
         POST = "POST"
@@ -13,9 +13,23 @@ class Resource(object):
         DELETE = "DELETE"
         OPTIONS = "OPTIONS"
 
+    _name = ""
+    rules = []
+
     @property
     def params(self):
         return request.params
+
+    @property
+    def name(self):
+        if self._name:
+            return self._name
+        else:
+            return self.__class__.__name__
+
+    @name.setter
+    def name(self, value):
+        self._name = value
 
     def getFunction(self, method: Methods):
         return getattr(self, method.value.lower(), None)
@@ -28,22 +42,27 @@ class Resource(object):
     def setRouteConfig(cls, method: Methods, cfg: dict) -> None:
         setattr(cls, method.value.upper(), cfg)
 
+
 class API(object):
-    name = 'bottle-rest'
+    name = "bottle-rest"
     api = 2
 
     def __init__(self, debug=False):
-         self.debug = debug
+        self.debug = debug
+        self.resources = []
 
     def setup(self, app: Bottle):
-        ''' Make sure that other installed plugins don't affect the same
-            keyword argument.'''
+        """Make sure that other installed plugins don't affect the same
+        keyword argument."""
         self.app = app
         for other in app.plugins:
-            if not isinstance(other, API): continue
+            if not isinstance(other, API):
+                continue
             if other.keyword == self.keyword:
-                raise PluginError("Found another sqlite plugin with "\
-                "conflicting settings (non-unique keyword).")
+                raise PluginError(
+                    "Found another sqlite plugin with "
+                    "conflicting settings (non-unique keyword)."
+                )
 
     def apply(self, callback, context: Route):
         def _wrapper(*args, **kwargs):
@@ -52,30 +71,41 @@ class API(object):
                 print(f"\033[96mbottle_rest:\033[0m path: {request.path}")
                 print(f"\033[96mbottle_rest:\033[0m args: {args}")
                 print(f"\033[96mbottle_rest:\033[0m kwargs: {kwargs}")
-                print(f"\033[96mbottle_rest:\033[0m url params: {list(request.params.items())}")
+                print(
+                    f"\033[96mbottle_rest:\033[0m url params: {list(request.params.items())}"
+                )
                 print(f"\033[96mbottle_rest:\033[0m method params: {params}")
             if params:
                 data = request.json
                 if not data:
                     data = {}
                 for name, val in params.items():
-                    if (name not in data) and (name not in kwargs) and (val.default == val.empty):
+                    if (
+                        (name not in data)
+                        and (name not in kwargs)
+                        and (val.default == val.empty)
+                    ):
                         response.status = 400
                         return {name: f"This feild is is required"}
                 return callback(**data, **kwargs)
             return callback()
+
         return _wrapper
 
-    def addResource(self, resource_cls: Type[Resource], *rules):
+    def addResource(self, resource: Union[Type[Resource], Resource], *rules):
         for rule in rules:
             if self.debug:
                 (f"Adding rescource '{rule}'")
-            resource = resource_cls()
-            setattr(self, resource_cls.__name__, resource)
+            if callable(resource):
+                resource = resource()
+            # setattr(self, resource_cls.__name__, resource) Not sure if this is still needed
+            self.resources.append(resource)
             for method in resource.Methods:
                 func = resource.getFunction(method)
                 cfg = resource.getRouteConfig(method)
                 if func:
                     if self.debug:
-                        print(f"\033[96mbottle_rest:\033[0m Creating route {method.value} {rule} for method {func}")
+                        print(
+                            f"\033[96mbottle_rest:\033[0m Creating route {method.value} {rule} for method {func}"
+                        )
                     self.app.route(rule, method.value, func, **cfg)
